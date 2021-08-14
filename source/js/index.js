c8b4eca8fe334bfcf258e942d3efa3d7a846a5ee
dmitry.buyan <buyandl@mail.ru>
2021-08-14 15:49:01 +0400

    декоратиные элементы главной страницы: мобильная и планшетная версии

--- source/js/index.js
+++ source/js/index.js
@@ -1,3 +1,9 @@
 const menu = document.querySelector('.menu');
+const menuToggleButton = menu.querySelector('.menu__toggle');
 
 menu.classList.remove('menu--no-js');
+menu.classList.add('menu--closed');
+
+menuToggleButton.addEventListener('click', function () {
+  menu.classList.toggle('menu--closed')
+});
