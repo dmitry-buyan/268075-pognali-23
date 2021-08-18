f7f33665cfe6a792d0ca731efb207534889236da
dmitry.buyan <buyandl@mail.ru>
2021-08-18 13:07:33 +0400

    декоративные элементы внутренней страницы "попутчики": мобильная версия

--- source/js/index.js
+++ source/js/index.js
@@ -1,10 +1,17 @@
 const page = document.querySelector('html');
-const menu = document.querySelector('.menu');
+const menu = page.querySelector('.menu');
+// const filter = page.querySelector('.filter');
 const menuToggleButton = menu.querySelector('.menu__toggle');
+// const filterToggleButton = filter.querySelector('.filter__toggle');
 
 page.classList.remove('no-js');
 menu.classList.add('menu--closed');
+// filter.classList.add('filter--closed');
 
 menuToggleButton.addEventListener('click', () => {
   menu.classList.toggle('menu--closed')
 });
+
+// filterToggleButton.addEventListener('click', () => {
+//   filter.classList.toggle('filter--closed')
+// });
